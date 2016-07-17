6869e81e3228e6af18b59789808decb8e9269cef
munesug <13ne032@ms.dendai.ac.jp>
2016-07-18 01:31:22 +0900

    Update kadai2.m

--- kadai2.m
+++ kadai2.m
@@ -5,7 +5,7 @@
 
 clear; % 変数のオールクリア
 
-ORG=imread('212.png'); % 原画像の入力
+ORG=imread('im-j63.png'); % 原画像の入力
 ORG = rgb2gray(ORG); colormap(gray); colorbar;
 imagesc(ORG); axis image; % 画像の表示
 pause; % 一時停止
